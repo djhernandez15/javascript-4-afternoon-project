2b7fb660825f4762368def6d1573b0b8e4fc5fd5
David Hernandez <djhernandez@Davids-MacBook-Air.local>
2019-03-22 15:35:08 -0500

    initial commit

--- classes.js
+++ classes.js
@@ -30,6 +30,17 @@
 */
 
 //Code Here
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget(){
+    return this.first_name + ' ' + this.last_name + ' ' + 'Widget';
+  }
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -48,6 +59,19 @@
 */
 
 //Code Here
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age, reports) {
+    super(first_name, last_name, email, age);
+    this.reports = [];
+  }
+  hire(employee){
+    return this.reports.push(employee);
+  }
+  fire(index){
+    return this.reports.splice(index, 1);
+  }
+
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -72,7 +96,37 @@
 */
 
 //Code Here
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age, reports, title, bonus){
+    super(first_name, last_name, email, age, reports);
+    this.title = 'Not a manager';
+    this.bonus = 0;
+  }
+  hire(employee){
+    super.hire(employee);
 
+    if (this.reports.length === 0){
+      return this.title = 'Not a manager';
+    } else if (this.reports.length > 0 && this.reports.length <= 3){
+      return this.title = 'Barely Manager';
+    } else if (this.reports.length >= 4 && this.reports.length <= 10) {
+      return this.title = 'Mostly Manager';
+    } else if (this.reports.length >= 11 && this.reports.length <= 50){
+      return this.title = 'Manager';
+    } else if (this.reports.length >= 51 && this.reports.length <= 100){
+      return this.title = 'Manager Plus';
+    } else if (this.reports.length > 100){
+      return this.title = 'Bestest Manager';
+    }
+    return this.reports;
+  }
+  fire(index){
+    super.fire(index)
+    if (index){
+      return this.bonus += 100;
+    }
+  }
+}
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
@@ -99,5 +153,24 @@
 */
 
 //Code Here
-
+class Machine {
+  constructor(widgets_made_count, wear_and_tear_count, needs_reboot){
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false;
+  }
+  makeWidgets(num) {
+    this.widgets_made_count += num;
+    return this.wear_and_tear_count += num / 50;
+  }
+  fixMachine(){
+    return this.needs_reboot = true;
+  }
+  reboot(){
+    return () => {
+      this.wear_and_tear_count -= 10;        
+      this.needs_reboot = false;
+    }
+  }
+}
 
